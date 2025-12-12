720291cb6afa1d97cd50a1ac59d391c76fb4a97c
Abhishek25suman <146339606+Abhishek25suman@users.noreply.github.com>
2025-12-12 15:55:43 +0530

    first commit

---
+++ Python/animals.py
@@ -0,0 +1,11 @@
+#--- 2D list- a list of list for different types of animals ---
+
+carnivores = ["lion", "tiger", "wolf"]
+herbivores = ["cow", "sheep", "deer"]
+omnivores = ["bear", "pig", "dog"] 
+
+animals = [carnivores, herbivores, omnivores]
+
+print(animals)
+print(animals[1])
+print(animals[1][1])
---
+++ Python/multiplication.py
@@ -0,0 +1,8 @@
+# --- Multiplication Program ---
+# This program takes two inputs a and b from the user and calculates their product.
+
+print("__Product of two number__")
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+product = a * b
+print("Product of two numbers is:", product)
