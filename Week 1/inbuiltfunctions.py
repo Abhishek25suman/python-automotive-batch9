d978bc616ade80faa63810513073c6421e899778
Abhishek25suman <146339606+Abhishek25suman@users.noreply.github.com>
2025-12-24 14:57:26 +0530

    add 2

--- Week 1/inbuiltfunctions.py
+++
@@ -1,9 +0,0 @@
-a=-98
-print(abs(a))
-
-def cal_speed(dist,time):
-    return dist/time
-#user defined
-def cal_speed(disp,time):
-    return disp/time
-
--- Week 1/package.py
+++
@@ -1,8 +0,0 @@
-#a package module is a dir. containing multiple modules
-
-#python---> package module --->
-#builtin.py
-#reuse.py
-
-from Python import builtin
-builtin.py
